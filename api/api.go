f69b307fd0698b5484c6a5bc8ab9a8562ef02d8d
Paul Waldo <paul.w.waldo@gmail.com>
2024-08-30 08:29:57 -0400

    Adds GetContactPhotos

--- api/api.go
+++ api/api.go
@@ -16,6 +16,11 @@ type Contact struct {
 }
 
 type Contacts struct {
+	// <contacts page="1" pages="1" perpage="1000" total="3">
+	//   <contact nsid="12037949629@N01" username="Eric" iconserver="1" realname="Eric Costello" friend="1" family="0" ignored="1" />
+	//   <contact nsid="12037949631@N01" username="neb" iconserver="1" realname="Ben Cerveny" friend="0" family="0" ignored="0" />
+	//   <contact nsid="41578656547@N01" username="cal_abc" iconserver="1" realname="Cal Henderson" friend="1" family="1" ignored="0" />
+	// </contacts>
 	Page    int       `xml:"page,attr"`
 	Pages   int       `xml:"pages,attr"`
 	PerPage int       `xml:"perpage,attr"`
@@ -33,10 +38,53 @@ func GetContactList(client *flickr.FlickrClient) (*GetContactListResponse, error
 	client.EndpointUrl = flickr.API_ENDPOINT
 
 	client.Args.Set("method", "flickr.contacts.getList")
-	// client.Args.Set("brand", "nikon")
 
 	client.OAuthSign()
 	response := &GetContactListResponse{}
+	err := flickr.DoGet(client, response)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return response, nil
+}
+
+// <photos>
+//   <photo id="2801" owner="12037949629@N01" secret="123456" server="1" username="Eric is the best" title="grease" />
+//   <photo id="2499" owner="33853651809@N01" secret="123456" server="1" username="cal18" title="36679_o" />
+//   <photo id="2437" owner="12037951898@N01" secret="123456" server="1" username="georgie parker" title="phoenix9_stewart" />
+// </photos>
+
+type Photo struct {
+	Id       string `xml:"id,attr"`
+	Owner    string `xml:"owner,attr"`
+	Secret   string `xml:"secret,attr"`
+	Server   string `xml:"server,attr"`
+	Username string `xml:"username,attr"`
+	Title    string `xml:"title,attr"`
+}
+
+type ContactPhotos struct {
+	Page          int     `xml:"page,attr"`
+	Pages         int     `xml:"pages,attr"`
+	PerPage       int     `xml:"perpage,attr"`
+	Total         int     `xml:"total,attr"`
+	ContactPhotos []Photo `xml:"photos"`
+}
+type GetContactPhotosResponse struct {
+	flickr.BasicResponse
+	Photos ContactPhotos `xml:"photos"`
+}
+
+func GetContactPhotos(client *flickr.FlickrClient) (*GetContactPhotosResponse, error) {
+	client.Init()
+	client.EndpointUrl = flickr.API_ENDPOINT
+
+	client.Args.Set("method", "flickr.photos.getContactsPhotos")
+
+	client.OAuthSign()
+	response := &GetContactPhotosResponse{}
 	err := flickr.DoGet(client, response)
 
 	if err != nil {
--- api/api_test.go
+++ api/api_test.go
@@ -58,3 +58,52 @@ func TestGetContactList(t *testing.T) {
 	// 	IsFamily: false,
 	// })
 }
+
+var contactPhotosBody = `<?xml version="1.0" encoding="utf-8"?>
+	<rsp stat="ok">
+		<photos page="1" pages="1" perpage="1000" total="3">
+		<photo id="2801" owner="12037949629@N01" secret="123456" server="1" username="Eric is the best" title="grease" />
+		<photo id="2499" owner="33853651809@N01" secret="123456" server="1" username="cal18" title="36679_o" />
+		<photo id="2437" owner="12037951898@N01" secret="123456" server="1" username="georgie parker" title="phoenix9_stewart" />
+		</photos>
+	</contacts>
+	</rsp>`
+
+func TestGetContactPhotos(t *testing.T) {
+	fclient := flickr.GetTestClient()
+	server, client := flickr.FlickrMock(200, contactPhotosBody, "text/xml")
+	defer server.Close()
+	fclient.HTTPClient = client
+
+	resp, err := api.GetContactPhotos(fclient)
+
+	assert.NoError(t, err)
+	assert.Equal(t, 3, len(resp.Photos.ContactPhotos), "Expected 3 contact photoss, but got %d", len(resp.Photos.ContactPhotos))
+
+	// flickr.Expect(t, err, nil)
+	// flickr.Expect(t, resp.Photos.Page, 2)
+	// flickr.Expect(t, resp.Photos.Pages, 89)
+	// flickr.Expect(t, resp.Photos.PerPage, 10)
+	// flickr.Expect(t, resp.Photos.Total, 881)
+	// flickr.Expect(t, len(resp.Photos.Photos), 4)
+	// flickr.Expect(t, resp.Photos.Photos[0], Photo{
+	// 	Id:       "2636",
+	// 	Owner:    "47058503995@N01",
+	// 	Secret:   "a123456",
+	// 	Server:   "2",
+	// 	Title:    "test_04",
+	// 	IsPublic: true,
+	// 	IsFriend: false,
+	// 	IsFamily: false,
+	// })
+	// flickr.Expect(t, resp.Photos.Photos[3], Photo{
+	// 	Id:       "2610",
+	// 	Owner:    "12037949754@N01",
+	// 	Secret:   "d123456",
+	// 	Server:   "2",
+	// 	Title:    "00_tall",
+	// 	IsPublic: true,
+	// 	IsFriend: false,
+	// 	IsFamily: false,
+	// })
+}
