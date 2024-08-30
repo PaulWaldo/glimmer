04df61cc7ed4d8552a2bb26335c4017ed4070549
Paul Waldo <paul.w.waldo@gmail.com>
2024-08-30 08:30:34 -0400

    Start using contact photos

--- ui/contactphotos.go
+++ ui/contactphotos.go
@@ -4,16 +4,20 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
+	"gopkg.in/masci/flickr.v3"
 )
 
 type contactPhotos struct {
 	ma        *myApp
 	container *fyne.Container
 	title     *widget.Label
+	photoList *widget.List
+	ptotos flickr.
 }
 
 func (p *contactPhotos) makeUI() fyne.CanvasObject {
 	p.title = widget.NewLabel("Contact Photos")
+	p.photoList=widget.NewList()
 	p.container = container.NewVBox(p.title)
 	return p.container
 }
