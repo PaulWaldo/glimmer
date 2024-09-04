4906bb2fd932ac360bf7990dde3d8f30bab18745
Paul Waldo <paul.w.waldo@gmail.com>
2024-09-04 06:48:19 -0400

    Attempt at GetFeed

--- ui/contactphotos.go
+++ ui/contactphotos.go
@@ -1,10 +1,15 @@
 package ui
 
 import (
+	"fmt"
+
 	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/storage"
 	"fyne.io/fyne/v2/widget"
 	"github.com/PaulWaldo/glimmer/api"
+	"gopkg.in/masci/flickr.v3/photos"
 )
 
 type contactPhotos struct {
@@ -29,6 +34,23 @@ func (p *contactPhotos) makeUI() fyne.CanvasObject {
 			photo := p.photos[index]
 			card.SetTitle(photo.Title)
 			card.SetSubTitle(photo.Username)
+
+			info, err := photos.GetInfo(p.ma.client, photo.Id, photo.Secret)
+			if err != nil {
+				fyne.LogError("Failed to get photo info", err)
+				return
+			}
+			fmt.Printf("%#v", info)
+			photoUrl := fmt.Sprintf("https://live.staticflickr.com/%s/%s_%s_%s.jpg", info.Photo.Server, info.Photo.Id, info.Photo.Secret, "z")
+			// Download the image at url and convert to a canvas.Image
+			// uri,err := url.Parse(photoUrl)
+			uri, err := storage.ParseURI(photoUrl)
+			if err != nil {
+				fyne.LogError("parsing url", err)
+			}
+			c:=canvas.NewImageFromURI(uri)
+			card.SetContent(c)
+
 		},
 	)
 	p.container = container.NewBorder(p.title, nil, nil, nil, p.photoList)
