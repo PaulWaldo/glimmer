fc6bdf1c89c12ff836f89e2d3b3e4987927d1a5c
Paul Waldo <paul.w.waldo@gmail.com>
2024-09-18 16:42:20 -0400

    Right-size the PhotoView

--- ui/photoview.go
+++ ui/photoview.go
@@ -2,6 +2,7 @@ package ui
 
 import (
 	"fmt"
+	"strconv"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/canvas"
@@ -23,8 +24,27 @@ func (p photoView) makeUI() (*fyne.Container, error) {
 		return nil, fmt.Errorf("getting sizes: %w", err)
 	}
 	fmt.Printf("access Info: %+v\n", pai)
-	// Find the largest photo.  Assumes they are listed is ascending order
-	info := pai.Sizes[len(pai.Sizes)-1]
+	// Find the smallest photo that is larger than the current window
+	photoIndex := -1
+	winSize := p.ma.window.Canvas().Size()
+	for i := range pai.Sizes {
+		picWidth, err := strconv.ParseFloat(pai.Sizes[i].Width, 32)
+		if err != nil {
+			return nil, fmt.Errorf("converting width '%s' to float: %w", pai.Sizes[i].Width, err)
+		}
+		picHeight, err := strconv.ParseFloat(pai.Sizes[i].Height, 32)
+		if err != nil {
+			return nil, fmt.Errorf("converting height '%s' to float: %w", pai.Sizes[i].Height, err)
+		}
+		if picWidth >= float64(winSize.Width) && picHeight >= float64(winSize.Height) {
+			photoIndex = i
+			break
+		}
+	}
+	if photoIndex == -1 {
+		return nil, fmt.Errorf("no suitable photo found")
+	}
+	info := pai.Sizes[photoIndex]
 	url := info.Source
 	uri, err := storage.ParseURI(url)
 	if err != nil {
