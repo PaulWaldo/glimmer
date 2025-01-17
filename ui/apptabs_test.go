2df3a4e96a11df05a9db3b06e3b6016aa721a29c
Your Name <you@example.com>
2025-01-17 07:19:20 -0500

    Removes AppTab type check

--- ui/apptabs_test.go
+++ ui/apptabs_test.go
@@ -3,7 +3,6 @@ package ui
 import (
 	"testing"
 
-	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"github.com/stretchr/testify/assert"
 )
@@ -11,13 +10,10 @@ import (
 func TestAppTabs_MakeUI(t *testing.T) {
 	ui := makeUI()
 
-	// Assert that the returned UI element is an *fyne.Container (AppTabs)
-	assert.IsType(t, &fyne.Container{}, ui)
-
 	// Type assert to AppTabs to access its methods and fields
-	appTabs := ui.(*fyne.Container).Objects[0].(*container.AppTabs)
+	appTabs := ui.Objects[0].(*container.AppTabs)
 
-    // Assert that the container has two children (the tabs)
+	// Assert that the container has two children (the tabs)
 	assert.Equal(t, 2, len(appTabs.Items))
 
 	// Assert that the tab labels are correct
