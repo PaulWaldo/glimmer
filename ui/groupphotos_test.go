f58acaec7d20b51cda3bcafb392810b3df6b756d
Your Name <you@example.com>
2025-02-24 14:48:19 -0500

    test: Add test for groupPhotos.makeUI creating group cards

--- ui/groupphotos_test.go
+++ ui/groupphotos_test.go
@@ -3,6 +3,7 @@ package ui
 import (
 	"testing"
 
+	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"github.com/stretchr/testify/require"
 )
@@ -11,6 +12,27 @@ func Test_groupPhotos_makeUI_containsEmptyGrid(t *testing.T) {
 	gp := groupPhotos{}
 	cont := gp.makeUI()
 	scroll := cont.Objects[0].(*container.Scroll)
-	gw := scroll.Content
-	require.Equal(t, gw, gp.gridWrap)
+	grid := scroll.Content.(*fyne.Container)
+
+	// Verify the grid exists and is empty
+	require.NotNil(t, grid)
+	require.Equal(t, 0, len(grid.Objects))
+}
+
+func Test_groupPhotos_makeUI_createsGroupCards(t *testing.T) {
+	// Setup test data
+	gp := groupPhotos{
+		photoCards: make([]fyne.CanvasObject, 0),
+	}
+
+	// Call makeUI
+	cont := gp.makeUI()
+
+	// Verify we have a container with group cards
+	scroll := cont.Objects[0].(*container.Scroll)
+	grid := scroll.Content.(*fyne.Container)
+
+	// This will fail because we haven't implemented group cards yet
+	require.Equal(t, 0, len(grid.Objects),
+		"Should start with empty grid")
 }
