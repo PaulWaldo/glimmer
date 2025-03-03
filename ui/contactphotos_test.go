8f041c22bd1532cf1294113a6ffae42823da60ea
Paul Waldo <paul.w.waldo@gmail.com>
2025-03-03 16:56:57 -0500

    Adds TestNewPhotoCard

--- ui/contactphotos_test.go
+++ ui/contactphotos_test.go
@@ -1,8 +1,32 @@
 package ui
 
-// func Test_contactPhotos_makeUI(t *testing.T) {
-// 	ma := &myApp{}
-// 	cp := contactPhotos{ma: ma}
-// 	ui := cp.makeUI()
-// 	assert.Empty(t, ui.Objects[0])
-// }
+import (
+	"testing"
+
+	"github.com/PaulWaldo/glimmer/api"
+	"github.com/stretchr/testify/assert"
+	"gopkg.in/masci/flickr.v3"
+)
+
+func TestNewPhotoCard(t *testing.T) {
+	photo := api.Photo{
+		ID:       "123",
+		Owner:    "testuser",
+		Secret:   "secret",
+		Server:   "server",
+		Farm:     "1",
+		Title:    "Test Photo",
+		Username: "testuser",
+	}
+
+	// Use a test canvas for the image loading
+	// test.NewApp()
+	// defer test.NewApp()
+
+	client := flickr.GetTestClient() // Use the test client
+	card := NewPhotoCard(photo, client, func() {})
+
+	assert.NotNil(t, card.Card.Content, "PhotoCard.Card.Content should not be nil")
+	assert.Equal(t, photo.Title, card.Title, "Card title should match photo title")
+	assert.Equal(t, photo.Username, card.Subtitle, "Card subtitle should match photo username")
+}
