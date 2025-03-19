be7d48a901436f11e4576caf7cc393c8a3809d42
Your Name <you@example.com>
2025-03-19 06:29:36 -0400

    restarting testing

--- ui/groupphotos_test.go
+++ ui/groupphotos_test.go
@@ -1,1 +1,29 @@
 package ui
+
+import (
+	"testing"
+
+	"fyne.io/fyne/v2"
+	"github.com/stretchr/testify/assert" // Added for assertions
+	"gopkg.in/masci/flickr.v3/groups"
+)
+
+func TestSetGroups(t *testing.T) {
+	p := &groupPhotosUI{
+		ma:       &myApp{}, // Initialize myApp
+		cardByID: make(map[string]*fyne.CanvasObject),
+	}
+	testGroups := []groups.Group{
+		{ID: "1", Name: "Group 1"},
+		{ID: "2", Name: "Group 2"},
+	}
+
+	p.setGroups(testGroups)
+
+	assert.Equal(t, len(testGroups), len(p.groupCards))
+	for _, group := range testGroups {
+		card := (*p.cardByID[group.ID]).(*GroupCard) // Type assertion
+		assert.NotNil(t, card)
+		assert.Equal(t, group.Name, card.Title)
+	}
+}
