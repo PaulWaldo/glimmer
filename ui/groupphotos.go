167a66e6c1bd0e529e11292c1eb0343e0f27b32e
Your Name <you@example.com>
2025-04-01 17:20:08 -0400

    refactor: Improve group photos UI with dynamic photo loading and layout

--- ui/groupphotos.go
+++ ui/groupphotos.go
@@ -44,22 +44,54 @@ func (p *groupPhotosUI) setGroups(groups []groups.Group) {
 	p.groupCards = make([]fyne.CanvasObject, 0, len(groups))
 	p.cardByID = make(map[string]*fyne.CanvasObject)
 
-	fmt.Println("User's group:")
+	fmt.Println("User's groups:")
 	for _, group := range groups {
 		fmt.Println(group.Name)
+		
+		// Create a container for the group's photos
+		photoContainer := container.NewGridWrap(fyne.NewSize(150, 150))
+		
+		// Get photos for this group
+		if groupPhotos, ok := p.ma.usersGroupPhotos[group.Nsid]; ok {
+			// Add up to 4 photos initially (or all if less than 4)
+			photosToShow := len(groupPhotos.Photos)
+			if photosToShow > 4 {
+				photosToShow = 4
+			}
+			
+			for i := 0; i < photosToShow; i++ {
+				if i < len(groupPhotos.Photos) {
+					photoCard := NewGroupPhotoCard(groupPhotos.Photos[i], group.Nsid, p.ma.client)
+					photoContainer.Add(photoCard)
+				}
+			}
+			
+			// Add "More..." button if there are more photos
+			if len(groupPhotos.Photos) > 4 {
+				moreButton := widget.NewButton("More...", func() {
+					// This will be implemented in Story 5
+					fmt.Println("More button clicked for group:", group.Name)
+				})
+				photoContainer.Add(moreButton)
+			}
+		}
+		
+		// Create the group card with the photo container
 		card := &GroupCard{
 			Card: widget.Card{
 				Title: group.Name,
+				Content: container.NewVBox(
+					photoContainer,
+					widget.NewButton("Collapse", func() {
+						// This will be implemented in Story 6
+						fmt.Println("Collapse button clicked for group:", group.Name)
+					}),
+				),
 			},
 			ma:        p.ma,
 			GroupName: group.Name,
 		}
-		// Get the photo info for each of the photos to be displayed in the group card from ma.usersGroupPhotos
-		for groupID, photoInfo := range p.ma.usersGroupPhotos {
-			// For each photo info, create a NewGroupPhoto card, passing in the photo data.  The Group Photo card should be responsible for downloading the image and displaying it
-			photoCard := NewGroupPhotoCard(photoInfo.Photos[0], groupID, p.ma.client)
-			card.Content = photoCard
-		}
+		
 		p.groupCards = append(p.groupCards, card)
 		cardObj := fyne.CanvasObject(card)
 		p.cardByID[group.Nsid] = &cardObj
@@ -75,36 +107,37 @@ func (p *groupPhotosUI) setGroups(groups []groups.Group) {
 // GroupPhotoCard represents a card displaying a photo from a group
 type GroupPhotoCard struct {
 	widget.Card
-	NSID   string
-	info   photos.PhotoInfo
 	photo  api.Photo
 	client *flickr.FlickrClient
-	tap    func()
+	info   photos.PhotoInfo
 }
-
-// var NewImageFromURI = canvas.NewImageFromURI
 
 // loadImage loads the image for a group photo card
 func (c *GroupPhotoCard) loadImage() {
-	// Load the image...
+	// Get photo info
 	resp, err := photos.GetInfo(c.client, c.photo.ID, c.photo.Secret)
 	if err != nil {
 		fyne.LogError("Failed to get photo info", err)
+		c.SetContent(widget.NewLabel("Failed to load image"))
 		return
 	}
+	
 	c.info = resp.Photo
-	photoUrl := fmt.Sprintf("https://live.staticflickr.com/%s/%s_%s_%s.jpg", c.info.Server, c.info.Id, c.info.Secret, "z")
+	
+	// Construct the photo URL
+	photoUrl := fmt.Sprintf("https://live.staticflickr.com/%s/%s_%s_%s.jpg", 
+		c.info.Server, c.info.Id, c.info.Secret, "z")
+	
+	// Parse the URL
 	uri, err := storage.ParseURI(photoUrl)
 	if err != nil {
 		fyne.LogError("parsing url", err)
-		c.Content = widget.NewLabel("Failed to load image")
+		c.SetContent(widget.NewLabel("Failed to parse URL"))
 		return
 	}
-
+	
+	// Create and set the image
 	image := canvas.NewImageFromURI(uri)
-	if image == nil || image.Resource == nil {
-		panic("Image is nil")
-	}
 	image.FillMode = canvas.ImageFillContain
 	c.SetContent(image)
 }
@@ -112,17 +145,19 @@ func (c *GroupPhotoCard) loadImage() {
 // NewGroupPhotoCard creates a new photo card for group photos
 func NewGroupPhotoCard(photo api.Photo, groupID string, client *flickr.FlickrClient) *GroupPhotoCard {
 	clone := api.CloneClient(client)
-	i := &GroupPhotoCard{
+	card := &GroupPhotoCard{
 		Card: widget.Card{
 			Title:    photo.Title,
 			Subtitle: photo.Username,
 			Content:  widget.NewProgressBarInfinite(),
 		},
-		NSID:   groupID,
 		photo:  photo,
 		client: clone,
 	}
-	i.ExtendBaseWidget(i)
-	go i.loadImage() // Start loading the image in a goroutine
-	return i
+	card.ExtendBaseWidget(card)
+	
+	// Start loading the image asynchronously
+	go card.loadImage()
+	
+	return card
 }
